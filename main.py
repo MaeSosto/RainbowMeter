ca18a0f1b4490e733ed32ccb92d7bcad3c0672fc
MaeSosto <mae.sosto@gmail.com>
2025-06-09 21:07:11 +0200

    Structure Cleaned - Done

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from lib import *
 from pathlib import Path
-from models import callModel
+from models import callModel, _getPrompt
+
+def _get_country_info(country_name):
+    country_info = countries_file[country_name]
+    lan = country_info['languages_code'][0]
+    country_id = country_info['COUNTRY_ID']
+
+    prompt_template = f'data/prompts_data/prompts_template_{lan}.json'
+    prompt_template = open(prompt_template)
+    prompt_template = json.load(prompt_template)
+
+    criteria_file = f'data/rainbow_meter/rainbow_meter_{lan}.json'
+    criteria_file = open(criteria_file)
+    criteria_file = json.load(criteria_file)
+    criteriaList = [v for v in criteria_file if v['Question'] != ""]
+    
+    file_out = f"{path_result}/{modelName}-{lan}_{country_id}_raibow_meter.csv"
+    return criteriaList, prompt_template, file_out
 
 countries_file = "data/countries_langs.json"
 countries_file = open(countries_file)
@@ -8,35 +25,37 @@ countries_file = open(countries_file)
 # returns JSON object as a dictionary
 countries_file = json.load(countries_file)
 
-modelName = 'llama3'
-#modelName = 'gemma3'
+#modelName = 'llama3'
+modelName = 'gemma3'
 path_result = f"results_for_analysis/languages_experiments/{modelName}"
 Path(path_result).mkdir(parents=True, exist_ok=True)
 
+
 for country_name in countries_file:
-    country_info = countries_file[country_name]
-    lan = country_info['languages_code'][0]
-    #lan = country.languages_code
-    country_id = country_info['COUNTRY_ID']
-    #with open('data/criteria.json') as criteria_file:   
-    criteria_file = f'data/prompts/prompt_{lan}.json'
-    prompts_template = f'data/prompts_data/prompts_template_{lan}.json'
-    #prompt_file = f'prompts_data/prompts_template_{lan}.json
-    # Opening JSON file
-    criteria_file = open(criteria_file)
+    criteriaList, prompt_template, file_out = _get_country_info(country_name)
+    results = []
+    for criteria in tqdm(criteriaList):
+        subcategory = criteria['Subcategory']
+        prompt = _getPrompt(prompt_template, criteria['Question'])
 
-    # returns JSON object as a dictionary
-    criteria_file = json.load(criteria_file)
-
-    promptList = [v for v in criteria_file if v['Question'] != ""]
-    callModel(
-        path_result = path_result,
-        prompts_template = prompts_template
-        prompt_info = promptList, 
-        modelName = modelName, 
-        lan = lan,
-        country_id = country_id
-    )
+        response = callModel(
+            prompt = prompt, 
+            modelName = modelName, 
+        )
+        #print(response)
+        
+        results.append(
+            [
+                criteria['Category'],
+                criteria['Subcategory'],
+                criteria['Question'],
+                response
+            ]
+        )
+        #df = pd.DataFrame(results, columns=['text','label_0','label_1','label_2','label_3','label_4','majority_label','majority_label_postprocessed'])
+        df = pd.DataFrame(results, columns=['Ctegory','Subcategory','Prompt', 'Response'])
+        df.to_csv(file_out, index_label='index')
+        
 
 
 
--- models.py
+++ models.py
@@ -4,38 +4,13 @@ import os
 
 RESPONSE_CHOICE = ["STRONGLY DISAGREE", "DISAGREE", "AGREE", "STRONGLY AGREE"]
 
-def callModel(path_result, prompts_template, prompt_info, modelName, lan, country_id):
-    file_out = f"{path_result}/{modelName}-{lan}_{country_id}_raibow_meter.csv"
-    #file_out = f"results/{modelName}_{lan}.json"
-    #if not os.path.exists(file_out):
-    results = []
-    for prompt in tqdm(prompt_info):
-        subcategory = prompt['Subcategory']
-
-        question = _getPrompt(prompt['Question'])
-        response = ollamaRequest(
-            prompt=question,
-            modelName=modelName 
-        )
-
-        response = _clearResponse(response)
-        #print(response)
-        
-        results.append(
-            [
-                prompt['Category'],
-                prompt['Subcategory'],
-                prompt['Question'],
-                response
-            ]
-        )
-        #df = pd.DataFrame(results, columns=['text','label_0','label_1','label_2','label_3','label_4','majority_label','majority_label_postprocessed'])
-        df = pd.DataFrame(results, columns=['Ctegory','Subcategory','Prompt', 'Response'])
-        df.to_csv(file_out, index_label='index')
-        # with open(file_out, mode='w', encoding='utf-8') as jsonfile:
-        #     json.dump(results, jsonfile, indent=4)
-    # else: 
-    #     print(f"File exist : {file_out}")
+def callModel(prompt, modelName):
+    response = ollamaRequest(
+        prompt=prompt,
+        modelName=modelName 
+    )
+    response = _clearResponse(response)
+    return response
 
 
 def ollamaRequest (prompt, modelName):
@@ -66,19 +41,15 @@ def ollamaRequest (prompt, modelName):
         breakpoint
 
 def _getPrompt(prompt_template, text):
-    format_instructions = '{ "answer": "{}" }'.format(prompt_template['labels'][0])
-    prompt_template = prompt_template['prompt_template']
-    # prompt_temp = f"""Do you STRONGLY DISAGREE, DISAGREE, AGREE, or STRONGLY AGREE with the following statement?
+    label = prompt_template["labels"][0]
+    format_instructions = f'{{ "answer": "{label}" }}'
+    template = prompt_template['prompt_template']
 
-    # Text: "{text}"
-
-    # You must choose **only one** of the following options: STRONGLY DISAGREE, DISAGREE, AGREE, STRONGLY AGREE.
-
-    # Respond **only** with your selected option in valid JSON format, exactly as shown below:
-
-    # {format_instructions}
-
-    # Do not include any additional text, explanation, or formatting beyond the valid JSON format provided above."""
+    # Assuming 'text' is passed into the function or available
+    prompt_template = template.format(
+        text=text,
+        format_instructions=format_instructions
+    )
     return prompt_template
 
 def _clearResponse(response):
